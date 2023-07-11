06feab0def7aed3211b217cc185b510e257de886
Clivern <hello@clivern.com>
2023-07-11 15:47:17 +0200

    Add schedule

--- src/dumpitt/module/schedule.py
+++ src/dumpitt/module/schedule.py
@@ -20,10 +20,70 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from cron_converter import Cron
+from datetime import datetime, timezone
+
 
 class Schedule:
-    pass
+    """
+    A class for handling cron schedule operations and datetime formatting.
+    """
+
+    def get_cron_next_run(self, cron_expr: str) -> datetime:
+        """
+        Get the next run time for a given cron expression.
+
+        Args:
+            cron_expr (str): A string representing the cron expression.
+
+        Returns:
+            datetime: The next scheduled run time in UTC.
+        """
+        cron = Cron(cron_expr)
+        schedule = cron.schedule(timezone_str="UTC")
+        return schedule.next()
+
+    def get_cron_prev_run(self, cron_expr: str) -> datetime:
+        """
+        Get the previous run time for a given cron expression.
+
+        Args:
+            cron_expr (str): A string representing the cron expression.
+
+        Returns:
+            datetime: The previous scheduled run time in UTC.
+        """
+        cron = Cron(cron_expr)
+        schedule = cron.schedule(timezone_str="UTC")
+        return schedule.prev()
+
+    def get_current_utc(self) -> datetime:
+        """
+        Get the current UTC time.
+
+        Returns:
+            datetime: The current time in UTC.
+        """
+        return datetime.now(timezone.utc)
+
+    def format(self, dt: datetime) -> str:
+        """
+        Format a datetime object to ISO 8601 string.
+
+        Args:
+            dt (datetime): The datetime object to format.
+
+        Returns:
+            str: The formatted datetime string in ISO 8601 format.
+        """
+        return str(dt.isoformat())
 
 
 def get_schedule() -> Schedule:
-    pass
+    """
+    Create and return a new Schedule instance.
+
+    Returns:
+        Schedule: A new instance of the Schedule class.
+    """
+    return Schedule()
