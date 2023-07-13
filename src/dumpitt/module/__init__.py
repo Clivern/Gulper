470de4e4e0170c02c363b4b79a08ce2926d79d60
Clivern <hello@clivern.com>
2023-07-13 17:05:46 +0200

    Add storage

--- src/dumpitt/module/__init__.py
+++ src/dumpitt/module/__init__.py
@@ -26,6 +26,6 @@ from .file_system import FileSystem, get_file_system
 from .config import Config, get_config
 from .sqlite import SQLiteClient, get_sqlite_client
 from .schedule import Schedule, get_schedule
-from .storage import LocalStorage, get_local_storage
+from .storage import LocalStorage, S3Storage, Storage, get_storage
 from .util import success
 from .util import error
